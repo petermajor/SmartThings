ed035a2a0fa8a7ac1d5f74342b2405b0784bc23c
Peter Major <peter@petermajor.co.uk>
2015-12-29 12:35:55 +0000

    Change fingerprint, simplify handler

--- devices/MCO-2-Button-Switch.groovy
+++ devices/MCO-2-Button-Switch.groovy
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2015 SmartThings
+ *  Copyright 2015 Peter Major
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -16,13 +16,11 @@ metadata {
         capability "Actuator"
         capability "Switch"
         capability "Refresh"
+        capability "Polling"
         capability "Configuration"
         capability "Zw Multichannel"
 
-        fingerprint inClusters: "0x60"
-        fingerprint inClusters: "0x60, 0x25"
-        fingerprint inClusters: "0x60, 0x26"
-        fingerprint inClusters: "0x5E, 0x59, 0x60, 0x8E"
+        fingerprint deviceId:"0x1001", inClusters: "0x20 0x25 0x27 0x60 0x72 0x85 0x86 0x8E 0xEF"
     }
 
     simulator {
@@ -56,7 +54,7 @@ def parse(String description) {
     if (description.startsWith("Err")) {
         result = createEvent(descriptionText:description, isStateChange:true)
     } else if (description != "updated") {
-        def cmd = zwave.parse(description, [0x20: 1, 0x84: 1, 0x98: 1, 0x56: 1, 0x60: 3, 0x8E: 2])
+        def cmd = zwave.parse(description, [0x20: 1, 0x25: 1, 0x60: 3, 0x8E: 2])
         if (cmd) {
             result = zwaveEvent(cmd)
         }
@@ -71,75 +69,7 @@ def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicReport cmd) {
         createEvent(name: "switch", value: "off")
     } else if (cmd.value == 255) {
         createEvent(name: "switch", value: "on")
-    } else {
-        [ createEvent(name: "switch", value: "on"), createEvent(name: "switchLevel", value: cmd.value) ]
     }
-}
-
-private List loadEndpointInfo() {
-    if (state.endpointInfo) {
-        state.endpointInfo
-    } else if (device.currentValue("epInfo")) {
-        fromJson(device.currentValue("epInfo"))
-    } else {
-        []
-    }
-}
-
-def zwaveEvent(physicalgraph.zwave.commands.multichannelv3.MultiChannelEndPointReport cmd) {
-    log.debug "MCO2-zwaveEvent-MultiChannelEndPointReport {$cmd}"
-    updateDataValue("endpoints", cmd.endPoints.toString())
-    if (!state.endpointInfo) {
-        state.endpointInfo = loadEndpointInfo()
-    }
-    if (state.endpointInfo.size() > cmd.endPoints) {
-        cmd.endpointInfo
-    }
-    state.endpointInfo = [null] * cmd.endPoints
-    //response(zwave.associationV2.associationGroupingsGet())
-    [ createEvent(name: "epInfo", value: util.toJson(state.endpointInfo), displayed: false, descriptionText:""),
-      response(zwave.multiChannelV3.multiChannelCapabilityGet(endPoint: 1)) ]
-}
-
-def zwaveEvent(physicalgraph.zwave.commands.multichannelv3.MultiChannelCapabilityReport cmd) {
-    log.debug "MCO2-zwaveEvent-MultiChannelCapabilityReport {$cmd}"
-    def result = []
-    def cmds = []
-    if(!state.endpointInfo) state.endpointInfo = []
-    state.endpointInfo[cmd.endPoint - 1] = cmd.format()[6..-1]
-    if (cmd.endPoint < getDataValue("endpoints").toInteger()) {
-        cmds = zwave.multiChannelV3.multiChannelCapabilityGet(endPoint: cmd.endPoint + 1).format()
-    } else {
-        log.debug "endpointInfo: ${state.endpointInfo.inspect()}"
-    }
-    result << createEvent(name: "epInfo", value: util.toJson(state.endpointInfo), displayed: false, descriptionText:"")
-    if(cmds) result << response(cmds)
-    result
-}
-
-def zwaveEvent(physicalgraph.zwave.commands.associationv2.AssociationGroupingsReport cmd) {
-    log.debug "MCO2-zwaveEvent-AssociationGroupingsReport {$cmd}"
-    state.groups = cmd.supportedGroupings
-    if (cmd.supportedGroupings > 1) {
-        [response(zwave.associationGrpInfoV1.associationGroupInfoGet(groupingIdentifier:2, listMode:1))]
-    }
-}
-
-def zwaveEvent(physicalgraph.zwave.commands.associationgrpinfov1.AssociationGroupInfoReport cmd) {
-    log.debug "MCO2-zwaveEvent-AssociationGroupInfoReport {$cmd}"
-    def cmds = []
-    /*for (def i = 0; i < cmd.groupCount; i++) {
-        def prof = cmd.payload[5 + (i * 7)]
-        def num = cmd.payload[3 + (i * 7)]
-        if (prof == 0x20 || prof == 0x31 || prof == 0x71) {
-            updateDataValue("agi$num", String.format("%02X%02X", *(cmd.payload[(7*i+5)..(7*i+6)])))
-            cmds << response(zwave.multiChannelAssociationV2.multiChannelAssociationSet(groupingIdentifier:num, nodeId:zwaveHubNodeId))
-        }
-    }*/
-    for (def i = 2; i <= state.groups; i++) {
-        cmds << response(zwave.multiChannelAssociationV2.multiChannelAssociationSet(groupingIdentifier:i, nodeId:zwaveHubNodeId))
-    }
-    cmds
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.multichannelv3.MultiChannelCmdEncap cmd) {
@@ -166,7 +96,7 @@ def on() {
 }
 
 def on(endpoint) {
-    log.debug "MCO2-on endpoint $endpoint"
+    log.debug "MCO2-on $endpoint"
     encap(zwave.basicV1.basicSet(value: 0xFF), endpoint)
 }
 
@@ -176,37 +106,30 @@ def off() {
 }
 
 def off(endpoint) {
-    log.debug "MCO2-off endpoint $endpoint"
+    log.debug "MCO2-off $endpoint"
     encap(zwave.basicV1.basicSet(value: 0x00), endpoint)
 }
 
 def refresh() {
     log.debug "MCO2-refresh"
-    command(zwave.basicV1.basicGet())
+    commands([
+        encap(zwave.basicV1.basicGet(), 1),
+        encap(zwave.basicV1.basicGet(), 2),
+        zwave.basicV1.basicGet()
+    ])
+}
+
+def poll(){
+    refresh()
 }
 
 def configure() {
     log.debug "MCO2-configure"
     enableEpEvents("1,2")
     commands([
-        //zwave.multiChannelAssociationV2.multiChannelAssociationRemove(groupingIdentifier:1, nodeId:[1]),
-        zwave.multiChannelAssociationV2.multiChannelAssociationGet(groupingIdentifier:1),
-        zwave.multiChannelAssociationV2.multiChannelAssociationGet(groupingIdentifier:2),
-        //zwave.multiChannelAssociationV2.multiChannelAssociationSet(groupingIdentifier:3, nodeId:[1]),
-        zwave.multiChannelAssociationV2.multiChannelAssociationGet(groupingIdentifier:3),
-        //zwave.multiChannelAssociationV2.multiChannelAssociationGroupingsGet()
-        zwave.multiChannelV3.multiChannelEndPointGet()
+        //zwave.multiChannelAssociationV2.multiChannelAssociationGet(groupingIdentifier:3),
+        zwave.multiChannelAssociationV2.multiChannelAssociationSet(groupingIdentifier:3, nodeId:zwaveHubNodeId)
     ], 800)
-}
-
-def epCmd(Integer ep, String cmds) {
-    log.debug "MCO2-epCmd {$ep} {$cmds}"
-    def result
-    if (cmds) {
-        def header = state.sec ? "988100600D00" : "600D00"
-        result = cmds.split(",").collect { cmd -> (cmd.startsWith("delay")) ? cmd : String.format("%s%02X%s", header, ep, cmd) }
-    }
-    result
 }
 
 def enableEpEvents(enabledEndpoints) {
@@ -216,11 +139,7 @@ def enableEpEvents(enabledEndpoints) {
 }
 
 private command(physicalgraph.zwave.Command cmd) {
-    if (state.sec) {
-        zwave.securityV1.securityMessageEncapsulation().encapsulate(cmd).format()
-    } else {
-        cmd.format()
-    }
+    cmd.format()
 }
 
 private commands(commands, delay=200) {
@@ -228,14 +147,10 @@ private commands(commands, delay=200) {
 }
 
 private encap(cmd, endpoint) {
-    log.debug "MCO2-encap {$endpoint} {$cmd}"
+    log.debug "MCO2-encap $endpoint {$cmd}"
     if (endpoint) {
         command(zwave.multiChannelV3.multiChannelCmdEncap(destinationEndPoint:endpoint).encapsulate(cmd))
     } else {
         command(cmd)
     }
-}
-
-private encapWithDelay(commands, endpoint, delay=200) {
-    delayBetween(commands.collect{ encap(it, endpoint) }, delay)
 }
