4242f3d5a423b236738c8529222b706a7de15887
Peter Major <peter@petermajor.co.uk>
2017-04-02 15:44:06 +0100

    Only refresh once

--- apps/iBrewManager.groovy
+++ apps/iBrewManager.groovy
@@ -1,7 +1,7 @@
 /**
  *  Generic UPnP Service Manager
  *
- *  Copyright 2016 SmartThings
+ *  Copyright 2016 Peter Major
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -36,84 +36,41 @@ preferences {
 
 def deviceDiscovery() {
 
-	discoverDevices()
-
-	def options = [:]
 	def devices = getDevices()
+	def options = [:]
 
-	devices.each {
-		def value = it.value.ip
-		def key = it.key
-		options["${key}"] = value
-	}
+	if (!devices) {
+		discoverDevices()
 
-	return dynamicPage(name: "deviceDiscovery", title: "Discovery Started!", nextPage: "", refreshInterval: 5, install: true, uninstall: true) {
-		section("Please wait while we discover your iBrew server. Discovery can take five minutes or more, so sit back and relax! Select your device below once discovered.") {
-			input "selectedDevices", "enum", required: false, title: "Select Devices (${options.size() ?: 0} found)", multiple: true, options: options
+		return dynamicPage(name: "deviceDiscovery", title: "Discovery Started...", nextPage: "", refreshInterval: 5, install: true, uninstall: true) {
+		}
+	
+	} else {
+
+		devices.each {
+			def value = it.value.ip
+			def key = it.key
+			options["${key}"] = value
+		}
+
+		return dynamicPage(name: "deviceDiscovery", title: "Discovery Finished", nextPage: "", install: true, uninstall: true) {
+			section("Found ${options.size() ?: 0} Smarter devices on the network") {
+				input "selectedDevices", "enum", required: false, title: "Select Devices", multiple: true, options: options
+			}
 		}
 	}
 }
 
 def installed() {
 	log.debug "Installed with settings: ${settings}"
-
-	//initialize()
 }
 
 def updated() {
 	log.debug "Updated with settings: ${settings}"
-
-	//unsubscribe()
-	//initialize()
 }
 
 def initialize() {
-	//unsubscribe()
-	//unschedule()
-
-	//ssdpSubscribe()
-
-	//if (selectedDevices) {
-	//	addDevices()
-	//}
-
-	//runEvery5Minutes("ssdpDiscover")
 }
-
-// void ssdpDiscover() {
-// 	sendHubCommand(new physicalgraph.device.HubAction("lan discovery ${searchTarget}", physicalgraph.device.Protocol.LAN))
-// }
-
-// void ssdpSubscribe() {
-// 	subscribe(location, "ssdpTerm.${searchTarget}", ssdpHandler)
-// }
-
-
-
-// Map verifiedDevices() {
-// 	def devices = getVerifiedDevices()
-// 	def map = [:]
-// 	devices.each {
-// 		def value = it.value.name ?: "UPnP Device ${it.value.ssdpUSN.split(':')[1][-3..-1]}"
-// 		def key = it.value.mac
-// 		map["${key}"] = value
-// 	}
-// 	map
-// }
-
-// void verifyDevices() {
-// 	def devices = getDevices().findAll { it?.value?.verified != true }
-// 	devices.each {
-// 		int port = convertHexToInt(it.value.deviceAddress)
-// 		String ip = convertHexToIP(it.value.networkAddress)
-// 		String host = "${ip}:${port}"
-// 		sendHubCommand(new physicalgraph.device.HubAction("""GET ${it.value.ssdpPath} HTTP/1.1\r\nHOST: $host\r\n\r\n""", physicalgraph.device.Protocol.LAN, host, [callback: deviceDescriptionHandler]))
-// 	}
-// }
-
-// def getVerifiedDevices() {
-// 	getDevices().findAll{ it.value.verified == true }
-// }
 
 def getDevices() {
 	if (!state.devices) {
@@ -153,71 +110,3 @@ void discoverDevicesCallback(physicalgraph.device.HubResponse hubResponse) {
 
  	log.debug "discoverDevicesCallback {$devices}"
 }
-
-
-// def addDevices() {
-// 	def devices = getDevices()
-
-// 	selectedDevices.each { dni ->
-// 		def selectedDevice = devices.find { it.value.mac == dni }
-// 		def d
-// 		if (selectedDevice) {
-// 			d = getChildDevices()?.find {
-// 				it.deviceNetworkId == selectedDevice.value.mac
-// 			}
-// 		}
-
-// 		if (!d) {
-// 			log.debug "Creating Generic UPnP Device with dni: ${selectedDevice.value.mac}"
-// 			addChildDevice("smartthings", "Generic UPnP Device", selectedDevice.value.mac, selectedDevice?.value.hub, [
-// 				"label": selectedDevice?.value?.name ?: "Generic UPnP Device",
-// 				"data": [
-// 					"mac": selectedDevice.value.mac,
-// 					"ip": selectedDevice.value.networkAddress,
-// 					"port": selectedDevice.value.deviceAddress
-// 				]
-// 			])
-// 		}
-// 	}
-// }
-
-// def ssdpHandler(evt) {
-// 	def description = evt.description
-// 	def hub = evt?.hubId
-
-// 	def parsedEvent = parseLanMessage(description)
-// 	parsedEvent << ["hub":hub]
-
-// 	def devices = getDevices()
-// 	String ssdpUSN = parsedEvent.ssdpUSN.toString()
-// 	if (devices."${ssdpUSN}") {
-// 		def d = devices."${ssdpUSN}"
-// 		if (d.networkAddress != parsedEvent.networkAddress || d.deviceAddress != parsedEvent.deviceAddress) {
-// 			d.networkAddress = parsedEvent.networkAddress
-// 			d.deviceAddress = parsedEvent.deviceAddress
-// 			def child = getChildDevice(parsedEvent.mac)
-// 			if (child) {
-// 				child.sync(parsedEvent.networkAddress, parsedEvent.deviceAddress)
-// 			}
-// 		}
-// 	} else {
-// 		devices << ["${ssdpUSN}": parsedEvent]
-// 	}
-// }
-
-// void deviceDescriptionHandler(physicalgraph.device.HubResponse hubResponse) {
-// 	def body = hubResponse.xml
-// 	def devices = getDevices()
-// 	def device = devices.find { it?.key?.contains(body?.device?.UDN?.text()) }
-// 	if (device) {
-// 		device.value << [name: body?.device?.roomName?.text(), model:body?.device?.modelName?.text(), serialNumber:body?.device?.serialNum?.text(), verified: true]
-// 	}
-// }
-
-// private Integer convertHexToInt(hex) {
-// 	Integer.parseInt(hex,16)
-// }
-
-// private String convertHexToIP(hex) {
-// 	[convertHexToInt(hex[0..1]),convertHexToInt(hex[2..3]),convertHexToInt(hex[4..5]),convertHexToInt(hex[6..7])].join(".")
-// }
