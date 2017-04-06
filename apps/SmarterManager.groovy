85fd2c2cd40b955105ffeed991357c627134cc67
Peter Major <peter@petermajor.co.uk>
2017-04-06 22:49:04 +0100

    Discover devices by mac address

--- apps/SmarterManager.groovy
+++ apps/SmarterManager.groovy
@@ -1,7 +1,7 @@
 /**
- *  Generic UPnP Service Manager
+ *  SmarterManager
  *
- *  Copyright 2016 Peter Major
+ *  Copyright 2017 Peter Major
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -48,13 +48,13 @@ def deviceDiscovery() {
 	} else {
 
 		devices.each {
-			def value = it.value.ip
-			def key = it.key
+			def value = "${it.value.type.description}"
+			def key = it.value.mac
 			options["${key}"] = value
 		}
 
 		return dynamicPage(name: "deviceDiscovery", title: "Discovery Finished", nextPage: "", install: true, uninstall: true) {
-			section("Found ${options.size() ?: 0} Smarter devices on the network") {
+			section("Found ${options.size() ?: 0} Smarter " + (options.size() == 1 ? "device" : "devices") + " on the network") {
 				input "selectedDevices", "enum", required: false, title: "Select Devices", multiple: true, options: options
 			}
 		}
@@ -84,7 +84,7 @@ def discoverDevices() {
  	log.debug "discoverDevices"
 
 	try {
-		def action = new physicalgraph.device.HubAction("""GET /api/devices HTTP/1.1\r\nHOST: $searchTarget\r\n\r\n""", physicalgraph.device.Protocol.LAN, searchTarget, [callback: discoverDevicesCallback]);
+		def action = new physicalgraph.device.HubAction("""GET /api/appliances HTTP/1.1\r\nHOST: $searchTarget\r\n\r\n""", physicalgraph.device.Protocol.LAN, searchTarget, [callback: discoverDevicesCallback]);
  		log.debug "action {$action}"
 		sendHubCommand(action)
 	}
@@ -99,13 +99,12 @@ void discoverDevicesCallback(physicalgraph.device.HubResponse hubResponse) {
 
 	def body = hubResponse.json
  	log.debug "body {$hubResponse.json}"
-	def remoteDevices = body.devices
 
 	def devices = getDevices()
 
-	remoteDevices.each {
-		if (!devices."${it?.ip}")
-			devices << ["${it?.ip}": it]
+	body.values().each {
+		if (!devices."${it?.mac}")
+			devices << ["${it?.mac}": it]
 	}
 
  	log.debug "discoverDevicesCallback {$devices}"
